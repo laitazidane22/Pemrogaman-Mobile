d67baee35ee2d8ba93f2fe03151bb63d9270e432
Laita Zidan <laitazidane@gmail.com>
2023-09-07 08:34:32 +0700

    Praktikum Mobile

--- Pertemuan 2/praktikum2/lib/main.dart
+++ Pertemuan 2/praktikum2/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp( const MyApp());
@@ -27,40 +28,40 @@ void main() {
 // }
 
 //-------SCAFFOLD----------
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  // int _count = 0;
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sample Code'),
-        ),
+//   // int _count = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Sample Code'),
+//         ),
 
-        body: Center(
-          child: Text('You have pressed the button times.')
-        ),
+//         body: Center(
+//           child: Text('You have pressed the button times.')
+//         ),
 
-        bottomNavigationBar: BottomAppBar (
-          child: Container(
-            height: 50.0,
-          ),
-        ),
+//         bottomNavigationBar: BottomAppBar (
+//           child: Container(
+//             height: 50.0,
+//           ),
+//         ),
 
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => 0,
-          tooltip: 'Increment Counter',
-          child: Icon(Icons.add),
-        ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () => 0,
+//           tooltip: 'Increment Counter',
+//           child: Icon(Icons.add),
+//         ),
 
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+//         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
-      ),
-    );
-  }
-}
+//       ),
+//     );
+//   }
+// }
 
 //-----------Dialog--------------
 // class MyApp extends StatelessWidget {
@@ -118,50 +119,79 @@ class MyApp extends StatelessWidget {
 //  );
 // }
 
+//INPUT DAN SELECTION WIDGET
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Contoh Text Field'),
+//         ),
+
+//         body: TextField(
+//           obscureText: false,
+//           decoration: InputDecoration(
+//             border: OutlineInputBorder(),
+//             labelText: 'Name'
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//DATE AND TIME PICKER
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contoh Date Picker',
+      home: MyHomePage(
+        title: 'Contoh Date Picker',
+      ),
+    );
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //Variable/State untuk mengambil tanggal
+  DateTime selectedDate = DateTime.now();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  // Initial SelectDate FLutter
+  Future<Null> _selectDate(BuildContext context) async {
+ // Initial DateTime FIinal Picked
+  final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101));
+  if (picked != null && picked != selectedDate)
+      setState(() {
+      selectedDate = picked;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
