2d677da6fb867d8f96b231187553a0c886a2e897
Laita Zidan <laitazidane@gmail.com>
2023-09-19 11:29:32 +0700

    Praktikum Mobile

--- Pertemuan 4/statetfull_widget_and_map/lib/main.dart
+++ Pertemuan 4/statetfull_widget_and_map/lib/main.dart
@@ -11,40 +11,133 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: Scaffold(
-      appBar: AppBar(
-        title: Text('Konverter Suhu'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Konverter Suhu'),
         ),
-
         body: TextField(
           obscureText: false,
           decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            label: Text('Masukkan Suhu')
-          ),
+              border: OutlineInputBorder(), label: Text('Masukkan Suhu')),
         ),
-
-        
-
       ),
     );
   }
 }
 
-class DropdownButton extends StatefulWidget {
-  const DropdownButton({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
   @override
-  State<DropdownButton> createState() => _DropdownButtonState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _DropdownButtonState extends State<DropdownButton>{
-  
+class _MyHomePageState extends State<MyHomePage> {
+  double _inputUser = 0;
+  double _kelvin = 0;
+  double _reamur = 0;
+  final inputController = TextEditingController();
+  String _newValue = "Kelvin";
+  double _result = 0;
+
+  var listItem = ["Kelvin", "Reamur"];
+
+  @override
+  Widget build(BuildContext context) {
+    DropdownButton<String>(
+      items: listItem.map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      value: _newValue,
+      onChanged: (String changeValue) {},
+    );
+    void dropdownOnChanged(String changeValue) {
+      setState(() {
+        _newValue = changeValue;
+      });
+    }
+
+    throw UnimplementedError();
+  }
 }
 
+class Result extends StatelessWidget {
+  const Result({
+    Key key,
+    @required this.result,
+  }) : super(key: key);
+  final double result;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Hasil",
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            result.toStringAsFixed(1),
+            style: TextStyle(fontSize: 30),
+          )
+        ],
+      ),
+    );
+    // );
+  }
+}
 
+@override
+Widget build(BuildContext context) {
+  // This method is rerun every time setState is called, for instance as done
+  // by the _incrementCounter method above.
+  //
+  // The Flutter framework has been optimized to make rerunning build methods
+  // fast, so that you can just rebuild anything that needs updating rather
+  // than having to individually change instances of widgets.
+  return Scaffold(
+    appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
 
+        ),
+    body: Center(
+      // Center is a layout widget. It takes a single child and positions it
+      // in the middle of the parent.
+      child: Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Invoke "debug painting" (press "p" in the console, choose the
+        // "Toggle Debug Paint" action from the Flutter Inspector in Android
+        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+        // to see the wireframe for each widget.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[],
+      ),
+    ),
+  );
+}
